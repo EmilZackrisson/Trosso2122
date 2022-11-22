a4c88d1617539de29c9cc84423fa2bbd67e9e092
Emil Zackrisson <emil.zackrisson@gmail.com>
2022-11-22 19:56:23 +0100

    test

--- server/index.test.js
+++ server/index.test.js
@@ -16,4 +16,25 @@ describe("LED JSON file", () => {
 	test("Check if LED JSON contains correct keys", () => {
 		expect(ledStates[0].source).toBe("Server");
 	});
+
+	test("Check if LEDS in JSON contains correct keys", () => {
+		// Removes first element in array
+		ledStates.splice(0, 1);
+
+		ledStates.map((led) => {
+			expect(led.id).not.toBeNaN();
+			expect(led.id).not.toBe(null);
+			expect(led.state).toBeFalsy();
+			expect(led.name).not.toBe(null);
+			expect(typeof led.name).toBe("string");
+			expect(typeof led.disabled).toBe("boolean");
+		});
+	});
+
+	test("Check if LEDS in JSON has correct pin", () => {
+		ledStates.map((led) => {
+			expect(led.id).toBeLessThanOrEqual(53);
+			expect(led.id).toBeGreaterThanOrEqual(2);
+		});
+	});
 });
