9c5417073c44d2da9488d5dc8517bf8e3df7876f
Emil Zackrisson <emil.zackrisson@gmail.com>
2022-12-14 09:44:20 +0100

    f

--- web/src/Leds.js
+++ web/src/Leds.js
@@ -1,28 +1,27 @@
-var leds = [
-	{
-		source: "Server",
-	},
-	{
-		id: 13,
-		state: false,
-		name: "Built In",
-		info: "Denna LED sitter direkt på Arduinon",
-		disabled: false,
-	},
-	{
-		id: 3,
-		state: false,
-		name: "Grön",
-		info: "Denna LED är grön och styrs via transistor",
-		disabled: true,
-	},
-	{
-		id: 2,
-		state: false,
-		name: "CW LED",
-		info: "Denna LED är kall vit och styrs via transistor",
-		disabled: false,
-	},
+var leds = [{
+        source: "Server",
+    },
+    {
+        id: 13,
+        state: false,
+        name: "Built In",
+        info: "Denna LED sitter direkt på Arduinon",
+        disabled: false,
+    },
+    {
+        id: 3,
+        state: false,
+        name: "Grön",
+        info: "Denna LED är grön och styrs via transistor",
+        disabled: true,
+    },
+    {
+        id: 2,
+        state: false,
+        name: "CW LED",
+        info: "Denna LED är kall vit och styrs via transistor",
+        disabled: false,
+    },
 ];
 
 export default leds;
