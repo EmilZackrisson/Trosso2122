015076bfb714408370d790604b2459b1e979b4a5
Emil Zackrisson <51415707+EmilZackrisson@users.noreply.github.com>
2023-03-02 13:10:57 +0100

    Webserial and other fixes (#39)
    * Working serial
    
    * Webserial on map

--- web/src/Dash.js
+++ web/src/Dash.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import client from "./config/websocketConfig";
 import Config from "./config/Config";
 import leds from "./Leds";
 import { useEffect } from "react";
@@ -7,153 +6,108 @@ import LedCard from "./components/LedCard";
 import Footer from "./components/Footer";
 
 function Dash() {
-  const [serialStatus, setSerialStatus] = useState("Serial: 🟥");
-  const [websocketStatus, setWebsocketStatus] = useState("Websocket: 🟥");
+  const [serialStatus, setSerialStatus] = useState("Serial: 🔴");
   const [allLed, setAllLeds] = useState([]);
+  const [controlLedEvent, setControlLedEvent] = useState("");
 
-  console.log(Config.demo, Config.serverIp);
+  // Serial
+  const filters = [{ usbVendorId: 6790, usbProductId: 29987 }];
+  var lineBuffer = "";
 
   useEffect(() => {
-    if (Config.demo) {
-      // leds.splice(0,1);
-      console.log(leds);
-      setAllLeds([...leds]);
+    console.log(leds);
+    setAllLeds([...leds]);
 
-      setSerialStatus("Serial: 🟨(DEMO)");
-      setWebsocketStatus("Websocket: 🟨(DEMO)");
+    if ("serial" in navigator) {
+      console.log("Serial is supported");
     }
   }, []);
 
-  if (!Config.demo) {
-    console.log("Running in production mode");
-    client.onopen = () => {
-      // console.log("WebSocket Client Connected");
-      setWebsocketStatus("Websocket: ✅");
-    };
-    client.onclose = function () {
-      console.log("echo-protocol Client Closed");
-      setWebsocketStatus("Websocket: 🟥");
-      setSerialStatus("Serial: 🟥");
-    };
-    client.onerror = function () {
-      console.log("Connection Error");
-      setWebsocketStatus("Websocket: 🟥 Error");
-      setSerialStatus("Serial: 🟥");
-    };
-    client.onmessage = (message) => {
-      console.log("message.data:", message.data);
-      if (message.data.includes("Websocket is up and running")) {
-        console.log("Websocket is up and running");
-        setWebsocketStatus("Websocket: ✅");
-      }
-      if (message.data.includes('serial":"Serial Ansluten!"')) {
-        console.log("Serial ansluten!");
-        setSerialStatus("Serial: ✅");
-        return;
-      } else if (message.data.includes("Serial ej ansluten")) {
-        console.log("Serial ej ansluten");
-        setSerialStatus("Serial: 🟥");
-        return;
-      }
-      if (message.data.includes("Server")) {
-        try {
-          const serverJson = JSON.parse(message.data);
-          serverJson.splice(0, 1);
-          setAllLeds([...serverJson]);
-        } catch (error) {
-          console.log(error);
-        }
-        // console.log("allLedTest", allLed);
-      }
-    };
-    function checkStatus() {
-      // client.send(JSON.stringify({ type: "checkStatus" }));
-      // console.log(client.readyState);
-      if (client.readyState !== client.W3CWebSocket.OPEN) {
-        console.log("CHECK STATUS", websocketStatus);
-        setSerialStatus("Serial: 🟥");
-        setAllLeds([]);
-        setInterval(function () {
-          // refresh page
-          window.location.reload();
-        }, 5000);
-      }
-    }
-    // setInterval(checkStatus, 5000);
-  }
-
   function controlLed(led) {
     console.log(led);
-    if (Config.demo) {
-      let tempArray = [...allLed];
-      let index = tempArray.findIndex(
-        (ledFromArray) => ledFromArray.id === led.id
-      );
-      tempArray[index].state = !tempArray[index].state;
-      setAllLeds([...tempArray]);
-    } else {
-      client.send(
-        JSON.stringify({
-          type: "lightControl",
-          ledId: led.id,
-          toState: !led.state,
-        })
-      );
-    }
+    let tempArray = [...allLed];
+    let index = tempArray.findIndex(
+      (ledFromArray) => ledFromArray.id === led.id
+    );
+    tempArray[index].state = !tempArray[index].state;
+    setAllLeds([...tempArray]);
   }
 
-  var emptyListMessageTitle;
-  var emptyListMessage;
-  var emptylistClass;
+  async function serial() {
+    const port = await navigator.serial.requestPort({ filters });
+    await port.open({ baudRate: 115200 });
+    setSerialStatus("Serial: 🟢");
+    console.log("Serial connected");
 
-  if (Config.demo) {
-    emptylistClass = "hidden";
-  } else {
-    if (allLed.length === 0) {
-      emptyListMessageTitle = "Anslut till servern";
-      emptyListMessage = "Listan med lampor är tom";
-      emptylistClass = "hidden";
-    }
-    if (serialStatus.includes("🟥")) {
-      emptyListMessageTitle = "Anslut servern till Arduino";
-      emptyListMessage = "Serial inte ansluten";
-    }
-    if (websocketStatus.includes("🟥")) {
-      emptyListMessageTitle = "Anslut till servern";
-      emptyListMessage = "Inte ansluten till servern med Websocket";
-      emptylistClass = "hidden";
-    } else {
-      emptylistClass = "hidden";
+    const reader = port.readable.getReader();
+
+    let value = "";
+
+    window.addEventListener("click", (event) => {
+      console.log(event);
+      const buttonText = event.target.innerText;
+      if (buttonText.includes("SLÅ AV")) {
+        value = "set," + event.target.id + ",1";
+      } else if (buttonText.includes("SLÅ PÅ")) {
+        value = "set," + event.target.id + ",0";
+      } else {
+        console.log("Could not to state from button text");
+        return;
+      }
+
+      console.log(value);
+      if (port && port.writable) {
+        // Convert the string to an ArrayBuffer.
+        var enc = new TextEncoder();
+
+        const bytes = new Uint8Array(enc.encode(value));
+
+        const writer = port.writable.getWriter();
+
+        writer.write(bytes);
+        writer.releaseLock();
+      }
+    });
+
+    // Listen to data coming from the serial device.
+    while (true) {
+      const { value, done } = await reader.read();
+      if (done) {
+        // Allow the serial port to be closed later.
+        reader.releaseLock();
+        break;
+      }
+      // value is a Uint8Array.
+      if (value.length === 1) {
+        lineBuffer += String.fromCharCode(value[0]);
+      } else {
+        lineBuffer += new TextDecoder().decode(value);
+        let text = lineBuffer;
+        lineBuffer = "";
+        console.log("Uint8Array: ", value);
+        console.log("TextDecoder: ", text);
+      }
     }
   }
 
   return (
     <>
       <div className="Dash text-white">
-        <section className="grid h-50 p-24 text-center bg-accent text-white">
+        <section className="dash-header grid h-50 p-24 text-center bg-accent text-white justify-center">
           <h1 className="text-4xl">Kontrollpanel</h1>
           <p>Här kan du styra hela Trossö</p>
+          <button onClick={serial} className="btn-primary">
+            Anslut serial
+          </button>
+          <p>{serialStatus}</p>
         </section>
         <section className="grid grid-flow-row md:grid-cols-3 gap-3 p-5 ">
-          <div className={emptylistClass}>
-            <h1>{emptyListMessageTitle}</h1>
-            <p>{emptyListMessage}</p>
-          </div>
-
           {allLed.map((led) => {
-            return (
-              <LedCard
-                key={led.id}
-                led={led}
-                serialStatus={serialStatus}
-                Config={Config}
-                controlLed={controlLed}
-              />
-            );
+            return <LedCard key={led.id} led={led} controlLed={controlLed} />;
           })}
         </section>
+        <Footer />
       </div>
-      <Footer serialStatus={serialStatus} websocketStatus={websocketStatus} />
     </>
   );
 }
